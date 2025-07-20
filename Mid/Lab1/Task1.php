4dfc48532e4e7f387983795f22f3032c7a9876ec
Salman5371 <salmanfarshi.sdk@gmail.com>
2025-07-20 21:30:11 +0600

    Task1.php

--- Mid/Lab1/Task1.php
+++ Mid/Lab1/Task1.php
@@ -1,1 +1,84 @@
+<!doctype input>
+<html>
+    <title>Lab Task</title>
+    <body>
+    <center>
+        <h1 style = "color : blue "> AIUB </h1>
+        <h2 style = "color : blue "> Registration Form </h2>
+        
+</center>
 
+
+<h3> Complete the Registration </h3>
+
+
+
+<table>
+<tr><td>
+
+Full Name:
+
+</td>
+<td>
+    <input type = "Text">
+</td>
+</tr>
+
+
+<tr><td>
+    
+Email: 
+
+</td>
+<td>
+    <input type = "Text">
+</td>
+</tr>
+
+<tr> <td>
+    GENDER
+</td>
+</tr>
+
+<td></td>
+
+<td>  <input type = "radio" name = "dse"> Male </td>
+<td>  <input type = "radio" name = "dse"> Other </td>
+
+</tr>
+
+<tr>
+
+<td>
+    LANGUAGE KNOWN</td>
+     <td></td>
+
+ <td>
+    <input type = "checkbox"> English
+    <input type = "checkbox"> Bangla
+    <input type = "checkbox"> Hindi
+</td>
+</tr>   
+
+<tr>
+    <td>Country </td>
+
+ <select>   
+
+ <option value=""> -select-</option>
+  <option value=""> Bangladesh</option>
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
